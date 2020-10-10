9e9ba18f19ef860b6bcd35a6700f89e7c0e2c73e
dotnaonweh <49785290+dotnaonweh@users.noreply.github.com>
2020-10-10 14:58:51 +0700

    Add files via upload

---
+++ adfin.py
@@ -0,0 +1,33 @@
+# Admin Finder CMS Lokomedia
+
+import requests
+import time
+
+logo = """
+  __       _           _         _____ _           _           
+ / _ \    | |         (_)       |  ___(_)         | |          
+/ /_\ \ __| |_ __ ___  _ _ __   | |_   _ _ __   __| | ___ _ __ 
+|  _  |/ _` | '_ ` _ \| | '_ \  |  _| | | '_ \ / _` |/ _ \ '__|
+| | | | (_| | | | | | | | | | | | |   | | | | | (_| |  __/ |   
+\_| |_/\__,_|_| |_| |_|_|_| |_| \_|   |_|_| |_|\__,_|\___|_|   
+"""
+print logo
+
+# input target nya
+target = raw_input("Target : ")
+# wordlist
+wordlist = open("wordlist.txt", "r")
+site = wordlist.read()
+ay = site.split('\n')
+
+#
+for i in ay:
+    url = target + "/"+ i
+    code = requests.get(str(url)).status_code   
+    if code == 200:
+        print ("FOUND : ") + url 
+        result = open("result.txt", "a")
+        result.write(url + "\n")
+        result.close()
+    else:
+        print ("NOT FOUND : ") + url
